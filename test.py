14fd532ca38820dbb485adcfb80d0f5d7d863c3f
Mathias Dannesbo <neic@neic.dk>
2018-12-12 09:24:40 +0100

    Solve as3 and half of as as5

--- test.py
+++ test.py
@@ -50,12 +50,46 @@ assert org_u in json.loads(org_gr.text)["results"][0]
 #  Magenta) active from 2017-01-01 (included) to 2018-03-14 (excluded). Consider which
 #  attributes and relations to set.
 
+EN_URL = SERVER + "organisation/organisationenhed"
+
+
+cph_val = {
+    "from": "2017-01-01",  # required
+    "from_included": True,
+    "to": "2019-03-14",  # required
+    "to_included": False,
+}
+
+cph_data = {
+    "attributter": {  # required
+        "organisationenhedegenskaber": [  # required
+            {
+                "brugervendtnoegle": "copenhagen",  # required
+                "enhedsnavn": "Copenhagen",
+                "virkning": cph_val,  # required
+            }
+        ]
+    },
+    "tilstande": {  # required
+        "organisationenhedgyldighed": [  # required
+            {"gyldighed": "Aktiv", "virkning": cph_val}  # required
+        ]
+    },
+}
+
+cph_r = requests.post(EN_URL, json=cph_data)
+cph_u = json.loads(cph_r.text)["uuid"]
+
+
 #  4. Create an organisationenhed called “Aarhus” (which should be a subunit of Magenta)
 #  active from 2018-01-01 (included) to 2019-09-01 (excluded). Consider which attributes
 #  and relations to set.
 
 #  5. Make a query searching for all organisationenheder in LoRa - confirm that
 #  Copenhagen and Aarhus exist in the system.
+
+en_gr = requests.get(EN_URL, params={"brugervendtnoegle": "%"})
+assert cph_u in json.loads(en_gr.text)["results"][0]
 
 #  6. Add an address to the org unit in Aarhus (valid within the period where the org
 #  unit is active).
